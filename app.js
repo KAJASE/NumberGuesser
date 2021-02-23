e4a1fb266bcf902311e1f103cbdf8a653934654f
Karen <kareninejapiassu@gmail.com>
2021-02-23 17:47:34 +0100

    Add validation

--- app.js
+++ app.js
@@ -19,11 +19,28 @@ guessBtn.addEventListener('click', function(){
     setMessage(`Please enter a number between ${min} and ${max}`, 'red');
   }
   if(guess === winningNum){
-    guessInput.disable = true; 
-    guessInput.style.borderColor = 'green';
-    setMessage(`${winningNum} is correct!`, 'green');
+    gameOver(true, `${winningNum} is correct, YOU WIN! Congratulation!`);
+  }else{
+    guessesLeft -= 1;
+
+    if(guessesLeft === 0){
+      gameOver(false, `Game over, you lost. The correct number was ${winningNum}`);
+    }else{
+      guessInput.style.borderColor = 'red';
+      guessInput.value = '';
+      setMessage(`${guess} is not correct, ${guessesLeft} guesses left`, 'red');
+    }
   }
 });
+
+function gameOver(won, msg){
+  let color;
+  won === true ? color = 'green' : color = 'red';
+  guessInput.disable = true; 
+  guessInput.style.borderColor = color;
+  message.style.color = color; 
+  setMessage(msg);
+}
 
 function setMessage(msg, color){
   message.style.color = color;
